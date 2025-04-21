143d61c3213acb2c7a3c58ec23f895248e1280c2
Sales App <xumitlama23@gmail.com>
2025-04-21 12:40:25 +0545

    Added enums for TaskPriority and TaskStatus

---
+++ src/Acme.TaskAndTimeTracker.Domain.Shared/Enums/TaskPriority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.Enum
+{
+    public enum TaskPriority
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+}
---
+++ src/Acme.TaskAndTimeTracker.Domain.Shared/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.Enum
+{
+    public enum TaskStatus
+    {
+        Pending = 0,
+        InProgress = 1,
+        Completed = 2,
+    }
+}
