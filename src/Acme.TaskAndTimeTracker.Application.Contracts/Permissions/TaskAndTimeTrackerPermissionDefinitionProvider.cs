8bd17680802b217819da67660be4839aa9a29630
Sales App <xumitlama23@gmail.com>
2025-04-21 12:41:09 +0545

    Updated permissions for new modules

--- src/Acme.TaskAndTimeTracker.Application.Contracts/Permissions/TaskAndTimeTrackerPermissionDefinitionProvider.cs
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/Permissions/TaskAndTimeTrackerPermissionDefinitionProvider.cs
@@ -9,10 +9,29 @@ public class TaskAndTimeTrackerPermissionDefinitionProvider : PermissionDefiniti
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(TaskAndTimeTrackerPermissions.GroupName);
+        var taskAndTimeTrackerGroup = context.AddGroup(TaskAndTimeTrackerPermissions.GroupName, L("Permission:TaskAndTimeTracker"));
 
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(TaskAndTimeTrackerPermissions.MyPermission1, L("Permission:MyPermission1"));
+        // Permissions for Project
+        var projectPermission = taskAndTimeTrackerGroup.AddPermission(TaskAndTimeTrackerPermissions.Projects.Default, L("Permission:Projects"));
+        projectPermission.AddChild(TaskAndTimeTrackerPermissions.Projects.Create, L("Permission:Create"));
+        projectPermission.AddChild(TaskAndTimeTrackerPermissions.Projects.Update, L("Permission:Update"));
+        projectPermission.AddChild(TaskAndTimeTrackerPermissions.Projects.Delete, L("Permission:Delete"));
+
+        // Permissions for ProjectTask
+        var projectTaskPermission = taskAndTimeTrackerGroup.AddPermission(TaskAndTimeTrackerPermissions.ProjectTasks.Default, L("Permission:ProjectTasks"));
+        projectTaskPermission.AddChild(TaskAndTimeTrackerPermissions.ProjectTasks.Create, L("Permission:Create"));
+        projectTaskPermission.AddChild(TaskAndTimeTrackerPermissions.ProjectTasks.Update, L("Permission:Update"));
+        projectTaskPermission.AddChild(TaskAndTimeTrackerPermissions.ProjectTasks.Delete, L("Permission:Delete"));
+
+        // Permissions for TimeEntry
+        var timeEntryPermission = taskAndTimeTrackerGroup.AddPermission(TaskAndTimeTrackerPermissions.TimeEntries.Default, L("Permission:TimeEntries"));
+        timeEntryPermission.AddChild(TaskAndTimeTrackerPermissions.TimeEntries.Create, L("Permission:Create"));
+        timeEntryPermission.AddChild(TaskAndTimeTrackerPermissions.TimeEntries.Update, L("Permission:Update"));
+        timeEntryPermission.AddChild(TaskAndTimeTrackerPermissions.TimeEntries.Delete, L("Permission:Delete"));
+
+        // Permissions for ReportResult
+        var reportPermission = taskAndTimeTrackerGroup.AddPermission(TaskAndTimeTrackerPermissions.Reports.Default, L("Permission:Reports"));
+        reportPermission.AddChild(TaskAndTimeTrackerPermissions.Reports.View, L("Permission:View"));
     }
 
     private static LocalizableString L(string name)
--- src/Acme.TaskAndTimeTracker.Application.Contracts/Permissions/TaskAndTimeTrackerPermissions.cs
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/Permissions/TaskAndTimeTrackerPermissions.cs
@@ -4,8 +4,33 @@ public static class TaskAndTimeTrackerPermissions
 {
     public const string GroupName = "TaskAndTimeTracker";
 
+    public static class Projects
+    {
+        public const string Default = GroupName + ".Projects";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+    }
 
-    
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class ProjectTasks
+    {
+        public const string Default = GroupName + ".ProjectTasks";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class TimeEntries
+    {
+        public const string Default = GroupName + ".TimeEntries";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class Reports
+    {
+        public const string Default = GroupName + ".Reports";
+        public const string View = Default + ".View";
+    }
 }
