6d3ca60623bfe4028c761cebb82f6e0eb7372c4f
Sales App <xumitlama23@gmail.com>
2025-04-24 18:32:10 +0545

    Updated ReportResultDto to include total hours and report details by user, project, and task.

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ReportResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class ReportResultDto
+    {
+        public Guid GroupId { get; set; }
+        public string GroupName { get; set; }
+        public float TotalHours { get; set; }
+        public string Title { get; set; }
+    }
+}
