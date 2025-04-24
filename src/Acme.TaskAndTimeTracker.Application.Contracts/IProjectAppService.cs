ea5e5f3642f8dd7faad3fe90e85ab689a4c26faa
Sales App <xumitlama23@gmail.com>
2025-04-24 12:19:07 +0545

    "Added interface for ProjectAppService"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/IProjectAppService.cs
@@ -0,0 +1,17 @@
+﻿using Acme.TaskAndTimeTracker.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker;
+
+public interface IProjectAppService
+{
+    Task<ProjectDto> CreateAsync(CreateUpdateProjectDto input);
+    Task DeleteAsync(Guid id);
+    Task<ProjectDto> GetAsync(Guid id);
+    Task<PagedResultDto<ProjectDto>> GetListAsync(ProjectFilterDto input);
+    Task<ProjectDto> UpdateAsync(Guid id, CreateUpdateProjectDto input);
+}
