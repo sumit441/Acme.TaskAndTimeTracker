6ecbe1f7cb3ca218383959b913a657502a196f7f
Sales App <xumitlama23@gmail.com>
2025-04-24 12:20:22 +0545

    "Added interface for TimeEntryAppService"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/ITimeEntryAppService.cs
@@ -0,0 +1,19 @@
+﻿using Acme.TaskAndTimeTracker.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Acme.TaskAndTimeTracker
+{
+    public interface ITimeEntryAppService : IApplicationService
+    {
+        Task<TimeEntryDto> CreateAsync(CreateUpdateTimeEntryDto input);
+        Task<TimeEntryDto> UpdateAsync(Guid id, CreateUpdateTimeEntryDto input);
+        Task DeleteAsync(Guid id);
+        Task<TimeEntryDto> GetAsync(Guid id);
+        Task<PagedResultDto<TimeEntryDto>> GetListAsync(TimeEntryFilterDto input);
+    }
+}
