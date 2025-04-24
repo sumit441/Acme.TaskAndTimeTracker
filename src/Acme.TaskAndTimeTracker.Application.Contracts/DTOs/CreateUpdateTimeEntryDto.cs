f8345107f1b5f031acbf1bf767bb91d6895183a3
Sales App <xumitlama23@gmail.com>
2025-04-24 12:12:50 +0545

    " Updated CreateUpdateProjectDto"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/CreateUpdateTimeEntryDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class CreateUpdateTimeEntryDto
+    {
+        [Required]
+        public Guid TaskId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public decimal LoggedHours { get; set; }
+        [Required]
+        public DateTime LogDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
