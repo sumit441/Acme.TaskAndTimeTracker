56e54a1bd56939bef8bd564c97639d22c39d035a
Sales App <xumitlama23@gmail.com>
2025-04-24 18:31:38 +0545

    Updated ReportFilterDto to support filtering by user, project, task, and date.

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ReportFilterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class ReportFilterDto
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid? ProjectId { get; set; }
+        public Guid? UserId { get; set; }
+    }
+}
