c4143efb5025095e06682aa0f49eecc7e93ca59d
Sales App <xumitlama23@gmail.com>
2025-04-24 13:03:46 +0545

    "Added Id property to TimeEntryDto for entity reference and data transfer"

--- src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/TimeEntryDto.cs
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/TimeEntryDto.cs
@@ -8,6 +8,7 @@ namespace Acme.TaskAndTimeTracker.DTOs
 {
     public class TimeEntryDto
     {
+        public Guid Id { get; set; }
         public Guid TaskId { get; set; }
         public string TaskTitle { get; set; }
         public Guid UserId { get; set; }
