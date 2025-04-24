3370611856ab165d3b97c6885b9927317ad998d3
Sales App <xumitlama23@gmail.com>
2025-04-24 12:15:06 +0545

    " Updated ProjectTaskFilterDto"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ProjectTaskFilterDto.cs
@@ -0,0 +1,16 @@
+﻿using Acme.TaskAndTimeTracker.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using TaskStatus = Acme.TaskAndTimeTracker.Enum.TaskStatus;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class ProjectTaskFilterDto: PagedAndSortedResultRequestDto
+    {
+        public string Filter { get; set; } // for general text search (Title, Description, ProjectName, etc.)
+    }
+}
