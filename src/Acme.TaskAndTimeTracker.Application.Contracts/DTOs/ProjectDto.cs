1e971bb4bb18ee8cc3bbac9fa5bfc64eb4dd0f5e
Sales App <xumitlama23@gmail.com>
2025-04-24 12:13:40 +0545

    "Updated ProjectDto"

--- src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ProjectDto.cs
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ProjectDto.cs
@@ -8,13 +8,9 @@ namespace Acme.TaskAndTimeTracker.DTOs
 {
     public class ProjectDto
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-    }
-    public class CreateProjectDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string CreatorName { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
