1110ecc5ce3c03bcfa91712ecab8270e93ffe612
Sales App <xumitlama23@gmail.com>
2025-04-24 12:26:32 +0545

    "Removed Id from TimeEntry entity"

--- src/Acme.TaskAndTimeTracker.Domain/TimeEntries/TimeEntry.cs
+++ src/Acme.TaskAndTimeTracker.Domain/TimeEntries/TimeEntry.cs
@@ -1,6 +1,7 @@
 ﻿using Acme.TaskAndTimeTracker.Tasks;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,18 @@ namespace Acme.TaskAndTimeTracker.TimeEntries
 {
     public class TimeEntry : FullAuditedEntity<Guid>
     {
+        [Required]
         public Guid TaskId { get; set; }
         public ProjectTask Task { get; set; }
+
+        [Required]
         public Guid UserId { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Logged hours must be a positive value.")]
         public decimal LoggedHours { get; set; }
         public DateTime LogDate { get; set; }
+
+        [StringLength(500, ErrorMessage = "Notes should not exceed 500 characters.")]
         public string Notes { get; set; }
 
         public TimeEntry(Guid id, Guid taskId, Guid userId, DateTime logDate, decimal loggedHours, string notes) : base(id)
