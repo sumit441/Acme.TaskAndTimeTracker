1384f40a1cbe7a4f7afe0f39aedc3a403e8e63cb
Sales App <xumitlama23@gmail.com>
2025-04-24 12:16:32 +0545

    " Updated TimeEntryReportDto"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/TimeEntryReportDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class TimeEntryReportDto
+    {
+        public string ProjectName { get; set; }
+        public string TaskTitle { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double DurationInHours { get; set; }
+        public Guid TimeEntryId { get; set; }
+        public DateTime LogDate { get; set; }
+        public decimal LoggedHours { get; set; }
+        public string Notes { get; set; }
+    }
+}
