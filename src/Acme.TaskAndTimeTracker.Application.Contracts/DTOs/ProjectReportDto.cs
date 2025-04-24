06a73376ca9aaa0c04818b5d7d07678def037d6f
Sales App <xumitlama23@gmail.com>
2025-04-24 12:14:24 +0545

    " Updated ProjectReportDto"

---
+++ src/Acme.TaskAndTimeTracker.Application.Contracts/DTOs/ProjectReportDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Acme.TaskAndTimeTracker.DTOs
+{
+    public class ProjectReportDto
+    {
+        public string Name { get; set; }
+        public int TotalTasks { get; set; }
+        public double TotalLoggedHours { get; set; }
+        public List<TaskReportDto> Tasks { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
