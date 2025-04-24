6bf18d9745f383ad88b9eea0d3b88a70be065d85
Sales App <xumitlama23@gmail.com>
2025-04-24 12:21:47 +0545

    "Added AuditAndValidation migration"

--- src/Acme.TaskAndTimeTracker.EntityFrameworkCore/Migrations/20250423071317_AddAuditAndValidation.Designer.cs
+++ src/Acme.TaskAndTimeTracker.EntityFrameworkCore/Migrations/20250423071317_AddAuditAndValidation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Acme.TaskAndTimeTracker.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Acme.TaskAndTimeTracker.Migrations
 {
     [DbContext(typeof(TaskAndTimeTrackerDbContext))]
-    partial class TaskAndTimeTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250423071317_AddAuditAndValidation")]
+    partial class AddAuditAndValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Acme.TaskAndTimeTracker.EntityFrameworkCore/Migrations/20250423071317_AddAuditAndValidation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.TaskAndTimeTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditAndValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "ProjectTasks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "ProjectTasks",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeleterId",
+                table: "ProjectTasks",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "ProjectTasks",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ProjectTasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "ProjectTasks",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifierId",
+                table: "ProjectTasks",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "ProjectTasks");
+        }
+    }
+}
